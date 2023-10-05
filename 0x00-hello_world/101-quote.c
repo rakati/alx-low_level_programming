8571650836046fc2fc98b2419b90533b86221e65
rakati <ouhaddou.nouredine@gmail.com>
2023-10-05 17:01:14 +0100

    C language First Dacy add ex100-101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Print a string with double quote
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	return (0);
+}
