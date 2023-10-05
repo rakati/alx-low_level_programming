130af1a112c2ae799539fdd372dbbd4b71f9937e
rakati <ouhaddou.nouredine@gmail.com>
2023-10-05 15:21:06 +0100

    C language First Dacy add ex4-5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * 4-puts - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
