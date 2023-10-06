c70b96e5b53e13c0733084d0311f53e9e6b74031
rakati <ouhaddou.nouredine@gmail.com>
2023-10-06 17:36:35 +0100

    Fix C language part 2 ex:9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char c = '0';
+	int c = 0;
 
-	while (c <= '9')
+	while (c <= 9)
 	{
-		putchar(c++);
-		if (c <= '9')
+		putchar('0' + c++);
+		if (c <= 9)
 		{
 			putchar(',');
 			putchar(' ');
