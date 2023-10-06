c8795c9010b1d7f42c1691c9ea536f1c067c507c
rakati <ouhaddou.nouredine@gmail.com>
2023-10-06 17:15:53 +0100

    Add C language part 2 ex:2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+		putchar(c++);
+	putchar('\n');
+	return (0);
+}
