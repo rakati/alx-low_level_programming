b2cc0b39b18efe0b6da45ae47e8c2c698b34c2be
rakati <ouhaddou.nouredine@gmail.com>
2023-10-10 20:30:24 +0100

    Add ex10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two integers.
+ *
+ * @a: integer
+ * @b: integer
+ * Return: (int) sum of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
