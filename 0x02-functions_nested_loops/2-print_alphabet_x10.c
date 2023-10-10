4e17e20d52440689095255344e2ac3e5cb93a47c
rakati <ouhaddou.nouredine@gmail.com>
2023-10-10 19:27:40 +0100

    add ex02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets to the standard output
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+			_putchar(c++);
+		_putchar('\n');
+		i++;
+	}
+}
