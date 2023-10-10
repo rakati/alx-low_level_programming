9826c4ce8b281c0b10f0c786e89be82bcef866c3
rakati <ouhaddou.nouredine@gmail.com>
2023-10-10 19:52:31 +0100

    fix code style ex5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - Check the sign of an integer
  *
- * @c: an integer to check its sign
+ * @n: an integer to check its sign
  * Return: (int)
  *	- (1): character is an alphabet
  *	- (0): character is not an alphabet
