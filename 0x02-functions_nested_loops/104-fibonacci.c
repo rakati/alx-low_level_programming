d2f29aa8f157d0c99946010f316c85f3d87402f8
rakati <ouhaddou.nouredine@gmail.com>
2023-10-10 22:32:55 +0100

    Add support for large numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,15 +8,25 @@
 int main(void)
 {
 	unsigned long int i = 1, j = 2, tmp;
+	unsigned long int ii = 0, jj = 0, t = 0;
 	int c = 0;
 
 	printf("1, 2");
 	while (c < 96)
 	{
-		printf(", %lu", i + j);
 		tmp = j;
+		t = jj;
+
 		j += i;
+		jj += ii + j / 100000;
+		j = j % 100000;
+
 		i = tmp;
+		ii = t;
+		if (jj != 0)
+			printf(", %lu%lu", jj, j % 100000);
+		else
+			printf(", %lu", j % 100000);
 		c++;
 	}
 	printf("\n");
