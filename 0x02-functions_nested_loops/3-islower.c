0e9eb2538875a89ded72b89f4005f5f2d7e23529
rakati <ouhaddou.nouredine@gmail.com>
2023-10-10 19:35:56 +0100

    add ex3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - Check if character is lowercase or not
+ *
+ * @c: integer represent ascii code of a character
+ * Return: (int)
+ *	- 1: character is lowercase
+ *	- 2: character is not lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
