3fa10462a4f53b3017b9da26a3602d9a75555eaa
rakati <ouhaddou.nouredine@gmail.com>
2023-10-12 17:58:17 +0100

    add func:print_numbers prints from 0..9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+		_putchar(c++);
+	_putchar('\n');
+}
