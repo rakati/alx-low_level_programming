85d797d9eeb33a8061922b5342226317d358550c
rakati <ouhaddou.nouredine@gmail.com>
2023-10-12 20:22:16 +0100

    add func: draw square using #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square using #.
+ *
+ * @size: integer - square side
+ */
+void print_square(int size)
+{
+	int i, j;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
