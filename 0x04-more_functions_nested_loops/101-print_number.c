6d20a16ff2d33e010e675ec24e82aced94f9bfc8
rakati <ouhaddou.nouredine@gmail.com>
2023-10-12 21:52:03 +0100

    fix print_number function

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,9 +10,8 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
 	}
 	if (n / 10 != 0)
-		print_number(n / 10);
-	_putchar('0' + n % 10);
+		print_number(n < 0 ? (n / 10) * -1 : n / 10);
+	_putchar('0' + (n < 0 ? (n % 10) * -1 : n % 10));
 }
