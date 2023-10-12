38b385f080fd745b270653871dec50785406b4fc
rakati <ouhaddou.nouredine@gmail.com>
2023-10-12 17:54:38 +0100

    Add mult function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -  multiplies two integers.
+ *
+ * @a: integer
+ * @b: integer
+ * Return: multiplication result of a x b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
