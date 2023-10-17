b911f3184c97f4ff92f8028d31063d613d9bbb72
rakati <ouhaddounouredine@gmail.com>
2023-10-17 03:44:23 +0100

    add ex8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print n elemennts
+ *
+ * @a: arry of integers
+ * @n: an integer
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		i++;
+		printf("%s", (i == n) ? "\n" : ", ");
+	}
+
+}
