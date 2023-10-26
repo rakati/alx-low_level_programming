1d92ac8e994fcb132f3cd77d3d03b5b3c2257d01
rakati <ouhaddou.nouredine@gmail.com>
2023-10-26 21:39:09 +0100

    add ex 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_sqrt - Calculate square root of a number recursively
+ * using binary search
+ *
+ * @l: An Integer represent low.
+ * @h: An Integer represent high.
+ * @n: An Integer to find its square root
+ * Return: Integer Square root of n
+ */
+static int binary_sqrt(int l, int h, int n)
+{
+	int mid;
+
+	if (l > h)
+		return (-1);
+	mid = (l + h) / 2;
+	if (mid * mid < n)
+		return (binary_sqrt(mid + 1, h, n));
+	else if (mid * mid > n)
+		return (binary_sqrt(l, mid - 1, n));
+	return (mid);
+}
+
+/**
+ * _sqrt_recursion - Calculate square root of a number recursively
+ * we will use binary search to find the square root in log(n)
+ *
+ * @n: An Integer.
+ * Return: Square root of an integer
+ */
+int _sqrt_recursion(int n)
+{
+	return (binary_sqrt(1, n, n));
+}
