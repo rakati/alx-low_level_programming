66ffdb1363745983940b4def0ca4deb0369f2f43
rakati <ouhaddou.nouredine@gmail.com>
2023-11-01 03:21:56 +0100

    add ex 101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * _strlen - len of string
+ *
+ * @s: arry of characters
+ * Return: len of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * strtow - splits a string into words.
+ * @str: Array of characters
+ *
+ * Return: pointer to 2d array of splitted string.
+ */
+char **strtow(char *str)
+{
+	int len = 0;
+	char **arr;
+	int i = 1, j = 0, z;
+
+	if (!str || !str[0])
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] == ' ' && str[i - 1] != ' ')
+			len++;
+		i++;
+	}
+	arr = malloc(sizeof(char *) * (len + 2));
+	if (!arr)
+		return (NULL);
+	arr[len] = NULL;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ')
+		{
+			len = 1;
+			while (str[len + i] && str[len + i] != ' ')
+				len++;
+			arr[j] = malloc(sizeof(char) * (len + 1));
+			for (z = 0; z < len; z++)
+				arr[j][z] = str[i++];
+			arr[j++][z] = '\0';
+		}
+		else
+			i++;
+	}
+	return (arr);
+}
