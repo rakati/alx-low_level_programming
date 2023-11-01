0bcb91177b159da067dadf1fbfabe13ae7a9f272
rakati <ouhaddou.nouredine@gmail.com>
2023-11-01 01:01:59 +0100

    add ex 02

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strlen - len of string
+ *
+ * @s: arry of characters
+ * Return: len of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * str_concat - concatenate two string
+ * @s1: array to concatenate
+ * @s2: array to concatenate
+ *
+ * Return: pointer to concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *arr;
+	int sz1, sz2, i;
+
+	sz1 = _strlen(s1);
+	sz2 = _strlen(s2);
+	arr = malloc((sz1 + sz2 + 1) * sizeof(char));
+	if (!arr)
+		return (NULL);
+	arr[sz1 + sz2] = '\0';
+	for (i = 0; s1[i]; i++)
+		arr[i] = s1[i];
+	for (; i < sz1 + sz2; i++)
+		arr[i] = s2[i - sz1];
+	return (arr);
+}
