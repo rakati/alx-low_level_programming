6d40d13cacbaeecd1b9eef09706efdc2fdefab61
rakati <ouhaddou.nouredine@gmail.com>
2023-11-01 01:10:55 +0100

    fix bug segfault ptr to Null ex 01

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,8 @@ int _strlen(char *s)
 {
 	int i;
 
+	if (!s)
+		return (0);
 	i = 0;
 	while (s[i] != '\0')
 	{
@@ -37,7 +39,7 @@ char *str_concat(char *s1, char *s2)
 	if (!arr)
 		return (NULL);
 	arr[sz1 + sz2] = '\0';
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < sz1; i++)
 		arr[i] = s1[i];
 	for (; i < sz1 + sz2; i++)
 		arr[i] = s2[i - sz1];
