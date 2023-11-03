c172b0e4fd44d102b7117eeaae84da5427ecfb2f
rakati <ouhaddou.nouredine@gmail.com>
2023-11-04 00:41:57 +0100

    fix ex 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _realloc -   reallocates a memory block using malloc and free
+ * _realloc - reallocates a memory block using malloc and free
  *
  * @ptr: pointer to old memory
  * @old_size: old size of old memory
@@ -35,5 +35,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		new_ptr[i] = ((char *)ptr)[i];
 		i++;
 	}
+	free(ptr);
 	return (new_ptr);
 }
