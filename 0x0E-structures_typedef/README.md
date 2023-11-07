e0b38ae5d448a56b6d28a65bf05db79a687dfc31
rakati <ouhaddou.nouredine@gmail.com>
2023-11-07 02:27:19 +0100

    add ex 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+# alx-low_level_programming
+Introduction to low level programming with C language
+## 0x0E. C - Structures, typedef
+
+**General:**
+- What are structures, when, why and how to use them
+- How to use `typedef`
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog info
+ * @name: string dog name
+ * @age: float dog age
+ * @owner: string owner name
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
