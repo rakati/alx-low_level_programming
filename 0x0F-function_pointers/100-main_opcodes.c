a2687a810ab7e5a28a8b70b817da368be1a5de33
rakati <ouhaddou.nouredine@gmail.com>
2023-11-08 19:47:53 +0100

    add ex100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that performs simple operations.
+ *
+ * @ac: integer represent number of args
+ * @av: array of program args
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int bytes;
+	int i = 0;
+	unsigned char *p;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	bytes = atoi(av[1]);
+	p = (unsigned char *)main;
+	while (i < bytes)
+		printf("%02x ", p[i++]);
+	printf("\n");
+	return (0);
+}
