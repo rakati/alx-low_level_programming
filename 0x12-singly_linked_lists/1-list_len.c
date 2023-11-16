b5038fb30406e01ba2f33a8bf20cd28d98044638
rakati <ouhaddounouredine@gmail.com>
2023-11-16 18:34:31 +0100

    add ex01 - list_len function

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len -  calculate number of elements in a linked `list_t` list
+ * @h: pointer to the head node of the list
+ *
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	int n = 0;
+	list_t *curr = (list_t *)h;
+
+	if (h == NULL)
+		return (0);
+	while (curr)
+	{
+		curr = curr->next;
+		n++;
+	}
+	return (n);
+}
