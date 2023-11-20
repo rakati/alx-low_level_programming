ef1e2a4369120f41deebec77462321786c683c7d
rakati <ouhaddounouredine@gmail.com>
2023-11-20 22:29:26 +0100

    fix case of inserting at the beginning of a list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,22 +15,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!head)
 		return (NULL);
+	if (*head == NULL && idx != 0)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
+	if (idx == 0)
+	{
+		new->next = (*head ? (*head)->next : NULL);
+		*head = new;
+		return (new);
+	}
 	curr = *head;
 	while (i < idx - 1 && curr)
 	{
 		curr = curr->next;
 		i++;
 	}
-	if (i != idx - 1 || !curr)
+	if (i != idx - 1)
 	{
 		free(new);
 		return (NULL);
 	}
 	new->next = curr->next;
 	curr->next = new;
-	return (curr);
+	return (new);
 }
