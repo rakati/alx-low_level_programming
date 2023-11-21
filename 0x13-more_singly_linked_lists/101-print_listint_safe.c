98e5aa260c57193d9fb9f11b51d50c3fe15f9d75
rakati <ouhaddounouredine@gmail.com>
2023-11-21 19:03:04 +0100

    print list & handle circular loop using floyd algo

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe -  prints all the elements of a listint_t list, handle
+ * circular list, detect circle and find first node of the circle.
+ * @head: A pointer to the head of the list
+ *
+ * Return: the number of nodes.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *slow, *fast;
+	int res;
+
+	if (!head)
+		return (98);
+	slow = (listint_t *)head;
+	fast = slow;
+	res = 0;
+	while (fast && fast->next)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		res++;
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			break;
+	}
+	if (slow != fast)
+		return (res);
+	slow = (listint_t *)head;
+	while (slow != fast)
+	{
+		printf("[%p] %d\n", (void *)fast, fast->n);
+		fast = fast->next;
+		slow = slow->next;
+		res++;
+	}
+	printf("-> [%p] %d\n", (void *)slow, slow->n);
+
+	return (res);
+}
