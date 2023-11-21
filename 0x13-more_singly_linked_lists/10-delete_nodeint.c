24bd1d3bfe20962fc5d4e0dc7ff7ff3de45abcf2
rakati <ouhaddounouredine@gmail.com>
2023-11-21 21:35:20 +0100

    null head considered as error ex10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *curr, *del;
 	unsigned int i = 0;
 
-	if (!head)
+	if (!head || *head == NULL)
 		return (-1);
 	if (index == 0)
 	{
