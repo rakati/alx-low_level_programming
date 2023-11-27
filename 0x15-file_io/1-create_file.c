d8067c08edff2fda5a523f7aff3ea3d8bcc040b7
rakati <ouhaddounouredine@gmail.com>
2023-11-27 21:10:55 +0100

    add ex1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen - len of string
+ *
+ * @s: array of characters
+ * Return: len of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * create_file - creat a file and add content to it
+ * @filename: a string represent path of the file to read.
+ * @text_content: NULL terminated string to write to the new created file
+ *
+ * Return: 1 on success otherwise -1 on error
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (write(fd, text_content, _strlen(text_content)) < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
