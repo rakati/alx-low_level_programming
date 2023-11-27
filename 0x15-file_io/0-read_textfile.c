4a5fd7df75a51f729b360228115de9e110d51fcc
rakati <ouhaddounouredine@gmail.com>
2023-11-27 20:52:15 +0100

    problem not found yet just restruct code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,18 +17,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
-	buff = malloc(letters + 1);
+	buff = malloc(letters * sizeof(char));
 	if (!buff)
 	{
 		close(fd);
 		return (0);
 	}
 	res = read(fd, buff, letters);
-	if (res >= 0)
-		res = write(1, buff, res);
+	if (res == -1)
+	{
+		close(fd);
+		free(buff);
+		return (0);
+	}
+	buff[res] = '\0';
+	res = write(STDOUT_FILENO, buff, res);
 	close(fd);
 	free(buff);
-	return (res < 0 ? 0 : res);
+	return (res);
 }
