fd1078673d00401b87f112c00effc8025fe57501
rakati <ouhaddounouredine@gmail.com>
2024-03-21 05:38:25 +0000

    correct iteration interval

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,14 +16,14 @@ def island_perimeter(grid):
             ones means Island, zeros means water.
     '''
     try:
-        l1 = len(grid) - 1
+        l1 = len(grid)
         res = 0
-        for i in range(1, l1):
-            for j in range(1, len(grid[i]) - 1):
+        for i in range(l1):
+            for j in range(len(grid[i])):
                 if grid[i][j]:
                     res += (i == 0 or grid[i - 1][j] == 0) + \
                         (j == 0 or grid[i][j - 1] == 0) +\
-                        (i == len(grid) - 1 or grid[i + 1][j] == 0) +\
+                        (i == l1 - 1 or grid[i + 1][j] == 0) +\
                         (j == len(grid[i]) - 1 or grid[i][j + 1] == 0)
         return res
     except ValueError:
